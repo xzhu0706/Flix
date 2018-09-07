b935586faf104e6844f79ace7684baed786e53ac
Xiaohong Zhu <xzhu0706@gmail.com>
2018-09-07 14:48:16 -0400

    finished required features

---
+++ Flix/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix
+//
+//  Created by Xiaohong Zhu on 9/5/18.
+//  Copyright © 2018 Xiaohong Zhu. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
